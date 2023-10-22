2d51f7a06d22c74b29b22e61258f3f8def19090a
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-10-22 02:49:42 +0200

    buff math match

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
 //generate random numbers. Utilizo la biblioteca Chance !
 // https://chancejs.com/basics/integer.html
 
-let firstNumber = chance.integer({ min: 0, max: 10 })
+//creamos la instancia chance y así usar sus métodos
 
-let secondNumber = chance.integer({ min: 0, max: 10 })
+const chance = new Chance();
+
+//Genero dos números aleatorios guandandolos cada uno en una variable
+
+let firstNumber = chance.integer({ min: 0, max: 10 });
+
+let secondNumber = chance.integer({ min: 0, max: 10 });
 
 //get the total
+
 let total = firstNumber + secondNumber;
+
+
+
+// numero aleatorio visible. SElecciono un elemento DOM  y actualizo su contenido
+
+const firstNumberNode = document.querySelector('#primary-number'); firstNumberNode.textContent = firstNumber;
+
+const secondNumberNode = document.querySelector('#secondary-number');
+secondNumberNode.textContent = secondNumber;
+
+
+const CheckButton = document.querySelector('#btn');
+CheckButton.addEventListener('click', function () {
+
+    const userGuessInput = document.querySelector('#guess');
+    const userGuess = parseInt(userGuessInput.value);
+
+
+    const messageNode = document.querySelector('#message');
+
+    if (total === userGuess) {
+        messageNode.textContent = '¡La suma es correcta!';
+
+    }
+    else {
+        messageNode.textContent = 'Lo siento, la suma no coincide!';
+
+    }
+});
+
+
+
+
